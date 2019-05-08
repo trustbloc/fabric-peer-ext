9530b178afc0f40d2015fc82d70cc881a5dad7f8
Firas Qutishat <firas.qutishat@securekey.com>
2019-05-08 14:52:08 -0400

    [#1] Add build structure
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

---
+++ extensions/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+func main() {
+	panic("not implemented yet")
+}
---
+++ scripts/pull_fabric.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright SecureKey Technologies Inc. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+set -e
+
+mkdir -p $GOPATH/src/github.com/hyperledger/fabric
+git clone https://github.com/trustbloc/fabric-mod.git $GOPATH/src/github.com/hyperledger/fabric
+cp -r . $GOPATH/src/github.com/hyperledger/fabric/fabric-peer-ext
+cd $GOPATH/src/github.com/hyperledger/fabric
+git config advice.detachedHead false
+# fabric-mod (May 7, 2019)
+git checkout 4beae6ee306bba2668b7b051e90433d57df3f9cd
+
+# Rewrite viper import to allow plugins to load different version of viper
+sed 's/\github.com\/spf13\/viper.*/github.com\/spf13\/oldviper v0.0.0/g' -i extensions/go.mod
+sed -e "\$areplace github.com/spf13/oldviper => github.com/spf13/viper v0.0.0-20150908122457-1967d93db724" -i extensions/go.mod
+sed 's/\github.com\/spf13\/viper.*/github.com\/spf13\/oldviper v0.0.0/g' -i go.mod
+sed -e "\$areplace github.com/spf13/oldviper => github.com/spf13/viper v0.0.0-20150908122457-1967d93db724" -i go.mod
+find . -type f -name "*.go" -print0 | xargs -0 sed -i "s/github.com\/spf13\/viper/github.com\/spf13\/oldviper/g"
