132498ba0a151291c9a183d048cac49f3d725646
Bob Stasyszyn <Bob.Stasyszyn@securekey.com>
2019-05-22 17:50:35 -0400

    fix: Fix bug in transient data retrieval
    Implement a handler for GetPrivateData that returns transient
    data if the collection type is transient.
    
    closes #108
    
    Signed-off-by: Bob Stasyszyn <Bob.Stasyszyn@securekey.com>

---
+++ pkg/collections/pvtdatahandler/pvtdatahandler.go
@@ -0,0 +1,100 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package pvtdatahandler
+
+import (
+	"context"
+
+	"github.com/hyperledger/fabric/common/flogging"
+	storeapi "github.com/hyperledger/fabric/extensions/collections/api/store"
+	"github.com/hyperledger/fabric/protos/common"
+	"github.com/trustbloc/fabric-peer-ext/pkg/config"
+)
+
+var logger = flogging.MustGetLogger("ext_pvtdatahandler")
+
+// Handler handles the retrieval of kevlar-defined collection types
+type Handler struct {
+	channelID        string
+	collDataProvider storeapi.Provider
+}
+
+// New returns a new Handler
+func New(channelID string, collDataProvider storeapi.Provider) *Handler {
+	return &Handler{
+		channelID:        channelID,
+		collDataProvider: collDataProvider,
+	}
+}
+
+// HandleGetPrivateData if the collection is one of the custom Kevlar collections then the private data is returned
+func (h *Handler) HandleGetPrivateData(txID, ns string, config *common.StaticCollectionConfig, key string) ([]byte, bool, error) {
+	switch config.Type {
+	case common.CollectionType_COL_TRANSIENT:
+		logger.Debugf("Collection [%s:%s] is of type TransientData. Returning transient data for key [%s]", ns, config.Name, key)
+		value, err := h.getTransientData(txID, ns, config.Name, key)
+		if err != nil {
+			return nil, true, err
+		}
+		return value, true, nil
+	default:
+		return nil, false, nil
+	}
+}
+
+// HandleGetPrivateDataMultipleKeys if the collection is one of the custom Kevlar collections then the private data is returned
+func (h *Handler) HandleGetPrivateDataMultipleKeys(txID, ns string, config *common.StaticCollectionConfig, keys []string) ([][]byte, bool, error) {
+	switch config.Type {
+	case common.CollectionType_COL_TRANSIENT:
+		logger.Debugf("Collection [%s:%s] is of type TransientData. Returning transient data for keys [%s]", ns, config.Name, keys)
+		values, err := h.getTransientDataMultipleKeys(txID, ns, config.Name, keys)
+		if err != nil {
+			return nil, true, err
+		}
+		return values, true, nil
+	default:
+		return nil, false, nil
+	}
+}
+
+func (h *Handler) getTransientData(txID, ns, coll, key string) ([]byte, error) {
+	ctxt, cancel := context.WithTimeout(context.Background(), config.GetTransientDataPullTimeout())
+	defer cancel()
+
+	v, err := h.collDataProvider.RetrieverForChannel(h.channelID).
+		GetTransientData(ctxt, storeapi.NewKey(txID, ns, coll, key))
+	if err != nil {
+		return nil, err
+	}
+
+	if v == nil {
+		return nil, nil
+	}
+
+	return v.Value, nil
+}
+
+func (h *Handler) getTransientDataMultipleKeys(txID, ns, coll string, keys []string) ([][]byte, error) {
+	ctxt, cancel := context.WithTimeout(context.Background(), config.GetTransientDataPullTimeout())
+	defer cancel()
+
+	vals, err := h.collDataProvider.RetrieverForChannel(h.channelID).
+		GetTransientDataMultipleKeys(ctxt, storeapi.NewMultiKey(txID, ns, coll, keys...))
+	if err != nil {
+		return nil, err
+	}
+
+	values := make([][]byte, len(vals))
+	for i, v := range vals {
+		if v == nil {
+			values[i] = nil
+		} else {
+			values[i] = v.Value
+		}
+	}
+	return values, nil
+}
---
+++ pkg/mocks/mockdataprovider.go
@@ -0,0 +1,58 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package mocks
+
+import (
+	"context"
+
+	storeapi "github.com/hyperledger/fabric/extensions/collections/api/store"
+)
+
+// DataProvider is a mock transient data provider
+type DataProvider struct {
+}
+
+// NewDataProvider returns a new Data Provider
+func NewDataProvider() *DataProvider {
+	return &DataProvider{}
+}
+
+// RetrieverForChannel returns the retriever for the given channel
+func (p *DataProvider) RetrieverForChannel(channel string) storeapi.Retriever {
+	return &dataRetriever{}
+}
+
+type dataRetriever struct {
+}
+
+// GetTransientData returns the transient data for the given context and key
+func (m *dataRetriever) GetTransientData(ctxt context.Context, key *storeapi.Key) (*storeapi.ExpiringValue, error) {
+	return &storeapi.ExpiringValue{Value: []byte(key.Key)}, nil
+}
+
+// GetTransientDataMultipleKeys returns the transient data with multiple keys for the given context and key
+func (m *dataRetriever) GetTransientDataMultipleKeys(ctxt context.Context, key *storeapi.MultiKey) (storeapi.ExpiringValues, error) {
+	values := make(storeapi.ExpiringValues, len(key.Keys))
+	for i, k := range key.Keys {
+		values[i] = &storeapi.ExpiringValue{Value: []byte(k)}
+	}
+	return values, nil
+}
+
+// GetData returns the data for the given context and key
+func (m *dataRetriever) GetData(ctxt context.Context, key *storeapi.Key) (*storeapi.ExpiringValue, error) {
+	return &storeapi.ExpiringValue{Value: []byte(key.Key)}, nil
+}
+
+// GetDataMultipleKeys returns the  data with multiple keys for the given context and key
+func (m *dataRetriever) GetDataMultipleKeys(ctxt context.Context, key *storeapi.MultiKey) (storeapi.ExpiringValues, error) {
+	values := make(storeapi.ExpiringValues, len(key.Keys))
+	for i, k := range key.Keys {
+		values[i] = &storeapi.ExpiringValue{Value: []byte(k)}
+	}
+	return values, nil
+}
